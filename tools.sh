8bf7885e39619c49e14c10f5b6e2f6a72ca332e6
Mark Wade <mark.wade@makeit.com>
2024-08-07 16:27:23 +0100

    Add UI

--- tools.sh
+++ tools.sh
@@ -1,5 +1,9 @@
-# Ensure to set `export ROOT_DIR=$PWD` and `. ./tools.sh` when in the root directory of the project before running the following commands
-function protoc() {
+export ROOT_DIR=$PWD
+
+# Use by first running `. ./tools.sh` when in the root directory of the project.
+# Then you can run the functions below
+
+function tools_protoc() {
     cd ${ROOT_DIR}
     rm -rf ${ROOT_DIR}/gen
     rm -rf ${MIT_PATH}/frontend/src/transport/client
